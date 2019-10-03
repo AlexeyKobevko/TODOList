4cf87354afabf912c9cf0964e682e417c646eb93
Alexey Kobevko <folco_2000@mail.ru>
2019-10-03 16:32:40 +0300

    добавил синхронизацию вкладок

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,11 @@ import { combineReducers } from 'redux';
 
 import { todosReducer} from './todos';
 import { userReducer } from "reducers/user";
+import { withReduxStateSync } from 'redux-state-sync';
 
 export const rootReducer = combineReducers({
   todos: todosReducer,
   user: userReducer,
 });
+
+export default withReduxStateSync(rootReducer);
--- src/store.js
+++ src/store.js
@@ -3,19 +3,24 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
-
+import { createStateSyncMiddleware, initStateWithPrevTab } from 'redux-state-sync';
 
 import { rootReducer } from './reducers';
-const persistConfig = {
-  key: 'root',
-  storage,
-};
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+// };
+//
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+const stateSync = createStateSyncMiddleware();
 
 export const store = createStore(
-  persistedReducer,
-  applyMiddleware(logger, thunk),
+  rootReducer,
+  applyMiddleware(logger, thunk, stateSync),
 );
+initStateWithPrevTab(store);
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
